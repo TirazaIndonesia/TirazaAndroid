98274ec20a41eb524578e263aee6588dd516cf6b
samuderaahr <samuderaalhakam@gmail.com>
2017-04-21 20:28:42 +0700

    import from old project

---
+++ app/src/main/java/com/tiraza/app/BaseActivity.java
@@ -0,0 +1,34 @@
+package tiraza.aplikasi;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+
+public class BaseActivity extends Activity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
