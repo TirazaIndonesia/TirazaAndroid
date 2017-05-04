15c6c75d3dac7e0de0c24880cade974a90a62a2c
samuderaahr <samuderaalhakam@gmail.com>
2017-05-04 20:38:43 +0700

    duh

--- app/src/main/java/com/tiraza/Splash.java
+++ app/src/main/java/com/tiraza/Splash.java
@@ -6,38 +6,31 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 
+
 public class Splash extends AppCompatActivity
 {
-  @Override
+  Thread logoTimer = new Thread()
+  {
+    public void run()
+    {
+      int logoTimer = 0;
+      int logoTimer2 = 0;
+      while (logoTimer < 300000000)
+      {
+        while (logoTimer < 300000000)
+        {
+          logoTimer2 = logoTimer2++;
+        }
+        logoTimer = logoTimer++;
+      }
+    }
+  };
+
+    @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
-    Thread logoTimer = new Thread()
-    {
-      public void run()
-      {
-        try
-        {
-          int logoTimer = 0;
-          while (logoTimer < 3000)
-          {
-            sleep(100);
-            logoTimer = logoTimer + 100;
-          }
-        }
-
-        catch (InterruptedException e)
-        {
-          e.printStackTrace();
-        }
-
-        finally
-        {
-          finish();
-        }
-      }
-    };
 
     logoTimer.start();
 
