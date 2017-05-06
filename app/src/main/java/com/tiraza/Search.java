dc15dfd92509fbddc4414c363c4dee4e2c0adee4
samuderaahr <samuderaalhakam@gmail.com>
2017-05-06 15:55:33 +0700

    fix

---
+++ app/src/main/java/com/tiraza/Search.java
@@ -0,0 +1,17 @@
+package com.tiraza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by agustio on 06/05/17.
+ */
+
+public class Search extends AppCompatActivity implements View.OnClickListener
+{
+  @Override
+  public void onClick(View v)
+  {
+
+  }
+}
